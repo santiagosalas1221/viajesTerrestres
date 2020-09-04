aea91cdc24e6226b5ed528d614836a0f61de465a
Santiago <santiagosalas.6532@gmail.com>
2020-09-03 20:30:22 -0500

    Correccion IDE

--- ViajesProject/src/Entidades/Municipio.java
+++ ViajesProject/src/Entidades/Municipio.java
@@ -10,5 +10,30 @@ package Entidades;
  * @author santi
  */
 public class Municipio {
-    
+
+    private int id_municipio;
+    private String nombre_Municipio;
+
+    public Municipio(int id_municipio, String nombre_Municipio) {
+        this.id_municipio = id_municipio;
+        this.nombre_Municipio = nombre_Municipio;
+    }
+
+
+    public int getId_municipio() {
+        return id_municipio;
+    }
+
+    public void setId_municipio(int id_municipio) {
+        this.id_municipio = id_municipio;
+    }
+
+    public String getNombre_Municipio() {
+        return nombre_Municipio;
+    }
+
+    public void setNombre_Municipio(String nombre_Municipio) {
+        this.nombre_Municipio = nombre_Municipio;
+    }
 }
+
